7eff2e50231d93a2f816c402f268bd0f9ecaa5ca
Travis Schmauss <travisschmauss@Traviss-M1-MBP.local>
2026-05-10 10:43:21 -0700

    Refresh Azir, Emperor of the Sands scrape

--- champions.js
+++ champions.js
@@ -3,7 +3,7 @@ window.__CHAMPIONS__ = [
     "slug": "azir-emperor",
     "name": "Azir, Emperor of the Sands",
     "url": "https://riftdecks.com/riftbound-decks?omni=azir%2C+emperor",
-    "scraped_at": "2026-05-10T07:21:23.612529Z",
+    "scraped_at": "2026-05-10T17:43:07.044105Z",
     "deck_count": 1292
   },
   {
