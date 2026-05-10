325c4abebc4ce9b16e64f63782bf9be351246a6b
Travis Schmauss <travisschmauss@Traviss-M1-MBP.local>
2026-05-10 10:10:47 -0700

    Refresh Lillia scrape (283 decks, 136 unique cards)

--- champions.js
+++ champions.js
@@ -24,7 +24,7 @@ window.__CHAMPIONS__ = [
     "slug": "lillia-bashful-bloom",
     "name": "Lillia, Bashful Bloom",
     "url": "https://riftdecks.com/legends/constructed/lillia-bashful-bloom?metagame_id=3",
-    "scraped_at": "2026-05-10T04:33:41.872495Z",
-    "deck_count": 279
+    "scraped_at": "2026-05-10T17:10:33.960215Z",
+    "deck_count": 283
   }
 ];
