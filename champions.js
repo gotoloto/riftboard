61bf21b91eeab113ba8003f6d6328d55cc03e243
Travis Schmauss <travisschmauss@Traviss-M1-MBP.local>
2026-05-12 08:22:06 -0700

    Add 8 legends: Diana, Kaisa, Master Yi (Wuju Master), MF, Pyke, Rengar, Sett, Jhin

--- champions.js
+++ champions.js
@@ -7,6 +7,13 @@ window.__CHAMPIONS__ = [
     "deck_count": 247
   },
   {
+    "slug": "diana-scorn-of-the-moon",
+    "name": "Diana, Scorn of the Moon",
+    "url": "https://riftdecks.com/legends/constructed/diana-scorn-of-the-moon?metagame_id=3",
+    "scraped_at": "2026-05-12T14:50:18.432783Z",
+    "deck_count": 319
+  },
+  {
     "slug": "fiora-grand-duelist",
     "name": "Fiora, Grand Duelist",
     "url": "https://riftdecks.com/legends/constructed/fiora-grand-duelist?metagame_id=3",
@@ -28,6 +35,20 @@ window.__CHAMPIONS__ = [
     "deck_count": 68
   },
   {
+    "slug": "jhin-virtuoso",
+    "name": "Jhin, Virtuoso",
+    "url": "https://riftdecks.com/legends/constructed/jhin-virtuoso?metagame_id=3",
+    "scraped_at": "2026-05-12T15:21:53.350666Z",
+    "deck_count": 74
+  },
+  {
+    "slug": "kaisa-daughter-of-the-void",
+    "name": "Kai'sa, Daughter of the Void",
+    "url": "https://riftdecks.com/legends/constructed/kaisa-daughter-of-the-void?metagame_id=3",
+    "scraped_at": "2026-05-12T14:56:18.577127Z",
+    "deck_count": 278
+  },
+  {
     "slug": "khazix-voidreaver",
     "name": "Khazix, Voidreaver",
     "url": "https://riftdecks.com/legends/constructed/khazix-voidreaver?metagame_id=3",
@@ -47,6 +68,41 @@ window.__CHAMPIONS__ = [
     "url": "https://riftdecks.com/legends/constructed/lillia-bashful-bloom?metagame_id=3",
     "scraped_at": "2026-05-12T14:36:42.484808Z",
     "deck_count": 303
+  },
+  {
+    "slug": "master-yi-wuju-master",
+    "name": "Master Yi, Wuju Master",
+    "url": "https://riftdecks.com/legends/constructed/master-yi-wuju-master?metagame_id=3",
+    "scraped_at": "2026-05-12T15:03:01.724366Z",
+    "deck_count": 227
+  },
+  {
+    "slug": "miss-fortune-bounty-hunter",
+    "name": "Miss Fortune, Bounty Hunter",
+    "url": "https://riftdecks.com/legends/constructed/miss-fortune-bounty-hunter?metagame_id=3",
+    "scraped_at": "2026-05-12T15:07:16.854141Z",
+    "deck_count": 210
+  },
+  {
+    "slug": "pyke-bloodharbor-ripper",
+    "name": "Pyke, Bloodharbor Ripper",
+    "url": "https://riftdecks.com/legends/constructed/pyke-bloodharbor-ripper?metagame_id=3",
+    "scraped_at": "2026-05-12T15:11:25.511739Z",
+    "deck_count": 133
+  },
+  {
+    "slug": "rengar-pridestalker",
+    "name": "Rengar, Pridestalker",
+    "url": "https://riftdecks.com/legends/constructed/rengar-pridestalker?metagame_id=3",
+    "scraped_at": "2026-05-12T15:15:28.238666Z",
+    "deck_count": 156
+  },
+  {
+    "slug": "sett-the-boss",
+    "name": "Sett, The Boss",
+    "url": "https://riftdecks.com/legends/constructed/sett-the-boss?metagame_id=3",
+    "scraped_at": "2026-05-12T15:18:45.318220Z",
+    "deck_count": 146
   },
   {
     "slug": "vex-gloomist",
